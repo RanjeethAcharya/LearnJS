85a18738b4009d387443c461ec6a289b98cdfc81
RanjeethAcharya <ranjeetkumar933342@gmail.com>
2025-03-21 00:59:22 +0530

    first day till arreys

--- learn.js
+++ learn.js
@@ -1,1 +1,32 @@
-console.log("hi fellow's")
+// console.log("hi fellow's")
+
+// let x = "ranjeeth";
+// console.log(x);
+// console.log(typeof(x));
+
+
+// Value types vs Reference type
+
+//value type
+// let x = "a";
+// let y = x;
+
+// x = "abcd";
+// console.log(x);
+// console.log(y);
+
+// reference type
+// let x = "a";
+// let y = x;
+
+// x.name = "abcd";
+// console.log(x);
+// console.log(y);
+
+
+
+
+
+
+
+
